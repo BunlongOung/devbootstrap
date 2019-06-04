50b7b6604448f2b2ebc44c74639d8501429199d0
DuchSamai <duchsamai77@gamil.com>
2019-06-04 12:04:21 +0700

    ui facebook

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
 import Calculator from './src/components/Calculator'
-
+import HomeScreen from './src/screens/HomeScreens/HomeScreen'
 export default class App extends Component {
   render() {
     return (
-      <Calculator />
+      // <Calculator/>
+      <HomeScreen/>
+      
     )
   }
 }
+
+
+
+
