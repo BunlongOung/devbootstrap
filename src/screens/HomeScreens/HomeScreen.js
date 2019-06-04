38178063391b3995693f5851e9fe093b539ac9a8
Bunlong <obunlong@gmail.com>
2019-06-04 13:40:28 +0700

    refactor

--- src/screens/HomeScreens/HomeScreen.js
+++ src/screens/HomeScreens/HomeScreen.js
@@ -1,74 +1,61 @@
 import React, { Component } from 'react'
 import { View, TextInput,Text,StyleSheet,TouchableOpacity,StatusBar} from 'react-native'
 
-
 class HomeScreen extends Component {
-    
-    render() {
-        return (
-           <View style = {styles.container}>
-               <StatusBar  barStyle="light-content"/>
-               <Text style={styles.text}>Facebook</Text>
+  render() {
+    return (
+       <View style = {styles.container}>
+          <StatusBar  barStyle="light-content"/>
+          <Text style={styles.text}>Facebook</Text>
 
-              <TextInput style={styles.input} placeholder = "Email"/>
-              
-              <TextInput style={styles.input} placeholder = "Password"/>
-               <TouchableOpacity>
-                   <View style={styles.controll_button}>
-                       <Text style={styles.text_button}>Login</Text>
-                   </View>
-               </TouchableOpacity>
-               
-           </View>
-        )
-     }
+          <TextInput style={styles.input} placeholder = "Email"/>
+
+          <TextInput style={styles.input} placeholder = "Password"/>
+          <TouchableOpacity>
+            <View style={styles.controll_button}>
+              <Text style={styles.text_button}>Login</Text>
+            </View>
+          </TouchableOpacity>
+       </View>
+      )
+   }
 }
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:'blue',
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        fontFamily:'sans-serif'
-        
-    },
-   
-    text:{
-        color:'#fff',
-       fontSize:40,
-       fontWeight:'bold',
-       
-    },
-    controll_button:{
-        
-       margin: 15,
-       height: 50,
-       width:300,
-       borderRadius:6,
-       borderColor: 'white',
-       borderWidth: 1
-    },
-    text_button:{
-        marginTop:10,
-        textAlign:'center',
-        fontSize:25,
-        color:'white'
-
-
-    },
-    
-    input: {
-       backgroundColor:'#fff',
-       padding:9,
-       margin: 15,
-       height: 50,
-       width:300,
-       borderRadius:6,
-       borderColor: 'white',
-       borderWidth: 1,
-       fontSize: 17
-    },
-    
-    
+  container: {
+    backgroundColor:'blue',
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  text:{
+    color:'#fff',
+    fontSize:40,
+    fontWeight:'bold',
+  },
+  controll_button:{
+    margin: 15,
+    height: 50,
+    width:300,
+    borderRadius:6,
+    borderColor: 'white',
+    borderWidth: 1
+  },
+  text_button:{
+    marginTop:10,
+    textAlign:'center',
+    fontSize:25,
+    color:'white'
+  },
+  input: {
+    backgroundColor:'#fff',
+    padding:9,
+    margin: 15,
+    height: 50,
+    width:300,
+    borderRadius: 6,
+    borderColor: 'white',
+    borderWidth: 1,
+    fontSize: 17
+  },
  })
 export default HomeScreen;
