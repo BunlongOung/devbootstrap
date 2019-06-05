24d47eb536b8a45029d939ad506fbec78c844404
measbona <measbona3@gmail.com>
2019-06-05 08:32:00 +0700

    finish authentication

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -5,21 +5,60 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  ActivityIndicator
 }from 'react-native'
 
 export default class Login extends Component{
+  state={
+    email: "",
+    password: "",
+    loading:false
+  }
+
+  onLogin = () => {
+    const {email,password} = this.state
+
+    if (password.length<6){
+      alert("Password must be at least 6 Character")
+      return
+    }
+
+    const endpoint = 'http://demo.oscarhq-test.com/api/v1/auth/sign_in'
+    const credentail = { email, password }
+
+    axios.post(endPoint, credentail)
+    .then(response => {
+      alert('Success')
+    })
+    .catch(error => {
+      alert('Invalid Log In')
+    })
+  }
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.container}>
         <View style={styles.text}>
           <Text>DevBootStrap</Text>
         </View>
-        <TextInput style={styles.box}/>
-        <TextInput style={styles.box}/>
+        <TextInput
+          style={styles.box}
+          placeholder="Email"
+          autoCapitalize="none"
+          textContentType="emailaddress"
+          onChangeText={text=> this.setState({email: text})}
+        />
+        <TextInput style={styles.box}
+          placeholder="Password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={text=> this.setState({password: text})}
+        />
         <TouchableOpacity style={styles.row}>
           <View>
-            <Text>Log In</Text>
+            {this.state.loading
+            ? <ActivityIndicator /> : <Text>Log In</Text>}
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
